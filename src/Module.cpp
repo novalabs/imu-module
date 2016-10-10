4d8a51f7fabcdccd765181da982ad8a23f2be4db
Davide Rizzi <d.rizzi@novalabs.io>
2016-10-10 16:07:58 +0200

    rtcan

--- src/Module.cpp
+++ src/Module.cpp
@@ -47,7 +47,7 @@ static const SPIConfig spi1cfg = {
 };
 
 static core::os::Thread::Stack<1024> management_thread_stack;
-static core::mw::RTCANTransport      rtcantra(RTCAND1);
+static core::mw::RTCANTransport      rtcantra(&RTCAND1);
 
 RTCANConfig rtcan_config = {
    1000000, 100, 60
